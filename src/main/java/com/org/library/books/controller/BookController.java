50091e1cb914b7a81332a370bde1ecb387fe95bd
gayamvenkat <83919940+gayamvenkat@users.noreply.github.com>
2021-05-12 03:01:19 +0530

    Add files via upload

--- src/main/java/com/org/library/books/controller/BookController.java
+++ src/main/java/com/org/library/books/controller/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -32,7 +33,7 @@ public class BookController {
 	private BooksService booksService;
 
 	@PostMapping("/uploadBooks")
-	public String uploadCSV(@RequestParam("file") MultipartFile file) {
+	public String uploadCSV(@RequestPart("file") MultipartFile file) {
 		return booksService.uploadCSV(file);
 
 	}
